5bc0560dd184fc5c23795474de1d40366468000f
caique <77406850+corqueeuvia@users.noreply.github.com>
2022-02-23 17:23:23 -0300

    made text animation function shorter

--- assets/scripts/homescript.js
+++ assets/scripts/homescript.js
@@ -1,20 +1,13 @@
+const text = document.getElementById("text").innerHTML;
+let i = 0;
+let speed = 50;
+
 // Typewriter text home
-
-var i = 0;
-
-var tag = document.getElementById("text");
-var html = document.getElementById("text").innerHTML;
-var attr = tag.setAttribute("data", html);
-var txt = tag.getAttribute("data");
-var speed = 50;
-
 function typeWriter() {
-  if (i <= txt.length) {
-    document.getElementById("text").innerHTML = txt.slice(0, i + 1);
+  if (i <= text.length) {
+    document.getElementById("text").innerHTML = text.slice(0, i + 1);
     i++;
     setTimeout(typeWriter, speed);
   }
-
 }
-
 typeWriter();
